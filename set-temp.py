da881ebd50fa1cca0ab9cc7d6cf4777729c9bd24
tmopencell <info@opencell.bio>
2020-02-26 12:56:15 +0000

    updated the .py file and created a www folder for displaying data

--- set-temp.py
+++ set-temp.py
@@ -14,8 +14,8 @@ import matplotlib.pylab as pylab
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 plt.switch_backend('agg')   #This is something copied from stackoverflow on the advice$
-path = '/home/pi/incubator/' #Set your path
-wwwpath = '/var/www/'
+path = '/home/pi/labtemplogger/' #Set your path
+wwwpath = '/home/pi/labtemplogger/www/'
 
 ####################################
 #### Extract Data write to file ####
@@ -56,6 +56,43 @@ elif 40 <= set_temp <= 80:
     print "Set Temperature:",set_temp
 else:
     print "User Set Temperature:",set_temp
+###############################
+## Save Data and Plot Figure ##
+###############################
+def savetempdataplotasgraph(date,exact_time,temperature,humidity):
+    # Write to a file
+    temphistory = open(wwwpath+'temphum-record.csv', 'a') # 'a' means append to file
+    w=csv.writer(temphistory)
+    fields=[date,exact_time,temperature,humidity]
+    w.writerow(fields)
+
+    # Starting to use pandas to read the data
+    df = pd.read_csv(wwwpath+'temphum-record.csv')
+    df.Time=pd.to_datetime(df.Time)
+    df.set_index('Time')
+    x = df['Time']
+    y1 = df['Temperature']
+    y2 = df['Humidity']
+    #print(x,y1,y2)
+    #######################
+    ##### PLOT THE DATA #####
+    #######################
+    # Plot parameters
+    pylab.rcParams['figure.figsize'] = 10, 8  # that's default image size for this interactive session
+    plt.plot(x, y1, x, y2)
+    plt.legend(['Temperature', 'Humidity'],fontsize = 'x-small', loc=2)
+    plt.grid(True) #puts dotted lines across chart (handy for analysis)
+    #plt.xlim(950,2000) #Preference Add limits on X-Y axis scale
+    #plt.ylim(0,4)
+    plt.axhline(y=set_temp, color='r', linestyle='--')
+    plt.tight_layout(pad=2.5, w_pad=2.5, h_pad=2.5)
+    # beautify the x-labels
+    plt.gcf().autofmt_xdate()
+    #Name and Axes labels
+    plt.xlabel('Date/Time', fontsize=13)
+    plt.ylabel('Temperature', fontsize=13)
+    plt.savefig(wwwpath+'temphum_graph.png')
+
 ##################################################
 ##### LOOP for finding optimal temperature ######
 ################################################
@@ -65,64 +102,26 @@ while True: #temperature != set_temp and i <= 20:
     humiditytemperature = Adafruit_DHT.read_retry(11,4) # Function for pulling the temp/humid data from DHT11
     temperature = humiditytemperature[1]
     humidity = humiditytemperature[0]
-    #print('python led_display.py %s' % int(temperature))
-    os.system('python led_display.py %s' % int(temperature))
+    date = now.strftime("%Y/%d/%m") #The function for writing the date and time
+    exact_time = now.strftime("%H:%M:%S")
     time.sleep(float(1)) # Wait for x seconds.
     # Take an image and place in the www directory
     if i==5:
-        print "Taking a photo"
-	os.system('python light_on.py')
-        os.system('sudo raspistill -o /var/www/incubator/static.jpg')
-        os.system('python light_off.py')
+        print "recording date, time and updating graph"
+        savetempdataplotasgraph(date,exact_time,temperature,humidity)
 	i = 0
     if temperature < set_temp:
-        print "Current Temperature:", temperature, "Target Temperature:", set_temp
-	os.system('python heater_on.py')
-        print "Heater ON"
+        print "Time: ", exact_time, "Current Temperature: ", temperature, "Target Temperature: ", set_temp
+        print "Too Cold!"
     elif temperature > set_temp:
-        print "Current Temperature:", temperature, "Target Temperature:", set_temp
-	os.system('python heater_off.py')
-        print "Heater OFF"
+        print "Time: ", exact_time, "Current Temperature: ", temperature, "Target Temperature: ", set_temp
+        print "Too Hot"
     else:
-        print "At Set Temperature Leaving Heater OFF"
-        print "Current Temperature:", temperature, "Target Temperature:", set_temp
-        os.system('python heater_off.py')
-        print "Heater OFF"
+        print "At Set Temperature"
+        print "Time: ", exact_time, "Current Temperature: ", temperature, "Target Temperature: ", set_temp
     #time.sleep(float(30)) # Wait for x seconds.
     i += 1
 print "Exiting Temperature Loop"
-
-# Write to a file
-temphistory = open(path+'temphum-record.csv', 'a') # 'a' means append to file
-w=csv.writer(temphistory)
-fields=[date,exact_time,temperature,humidity]
-w.writerow(fields)
-
-# Starting to use pandas to read the data
-df = pd.read_csv(path+'temphum-record.csv')
-df.Time=pd.to_datetime(df.Time)
-df.set_index('Time')
-x = df['Time']
-y1 = df['Temperature']
-y2 = df['Humidity']
-#print(x,y1,y2)
-#######################
-##### PLOT THE DATA #####
-#######################
-# Plot parameters
-pylab.rcParams['figure.figsize'] = 10, 8  # that's default image size for this interactive session
-plt.plot(x, y1, x, y2)
-plt.legend(['Temperature', 'Humidity'],fontsize = 'x-small', loc=2)
-plt.grid(True) #puts dotted lines across chart (handy for analysis)
-#plt.xlim(950,2000) #Preference Add limits on X-Y axis scale
-#plt.ylim(0,4)
-plt.tight_layout(pad=2.5, w_pad=2.5, h_pad=2.5)
-# beautify the x-labels
-plt.gcf().autofmt_xdate()
-#Name and Axes labels
-plt.xlabel('Date/Time', fontsize=13)
-plt.ylabel('Temperature', fontsize=13)
-plt.savefig(wwwpath+'incubator/temphum_graph.png')
 #####
 #END#
 #####
